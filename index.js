2b812569acd41254c85057e8d6934ad275af8e26
Andrea Abshire <sashady1488@gmail.com>
2018-07-30 19:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays,  ${name}!`
+  return `Happy holidays, ${name}!`
 }
 happyHolidaysTo ("you")
 
 function happyHolidayTo (holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
 happyHolidayTo("Independence Day", "you")
 
